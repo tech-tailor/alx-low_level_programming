dc6c026bce4e0a695973d2d1a128da87405f93a9
tech-tailor <ogundareakinniyi8@gmail.com>
2023-04-17 14:53:51 -0700

    update

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main : print source code file
+ *
+ * descripton: prints
+ *
+ * Return: success
+ */
+
+int main ()
+{
+	printf("%s\n", __FILE__);
+	
+	return (0);
+}
