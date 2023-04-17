10cfdc5ee3049fe0ee5f1e208593155ec8891858
tech-tailor <ogundareakinniyi8@gmail.com>
2023-04-17 15:42:45 -0700

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-
 /**
- * main: print source code file
+ * main - check the code for Alx School students.
  *
- * descripton: prints code file
- *
- * Return: success
- *
+ * Return: Always 0.
  */
 
 int main(void)
