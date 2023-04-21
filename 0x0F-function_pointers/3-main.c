f38ef5f71c45a982806ef66ee3f3844983608ae7
tech-tailor <ogundareakinniyi8@gmail.com>
2023-04-21 09:00:45 -0700

    updste

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,3 +10,27 @@
 */
 int main(int argc, char *argv[])
 {
+	int n1, n2, result;
+	int (*p)(int, int);
+
+	if (argc < 4 || argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+
+	p = get_op_func(argv[2]);
+
+	if (p == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = p(n1, n2);
+
+	printf("%d\n", result);
+	return (0);
+}
