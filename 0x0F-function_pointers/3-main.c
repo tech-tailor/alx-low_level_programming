72725acefab4ca4e8c5d1a1192954da23767af27
tech-tailor <ogundareakinniyi8@gmail.com>
2023-04-21 08:47:14 -0700

    a program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,12 @@
+#include "3-calc.h"
+/**
+ * main - Entry point
+ * @argc: the number of the parameters
+ * @argv: the parameters in the case the number to be calculated.
+(* a blank line
+* Description: this program is the enttry point for a calculator)?
+(* section header: 3-calc.h)*
+* Return: 0 in success
+*/
+int main(int argc, char *argv[])
+{
