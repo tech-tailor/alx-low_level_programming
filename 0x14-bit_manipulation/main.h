cdc615f0b21fcf80b3cd09ea04bcd4c2be7ae283
tech-tailor <ogundareakinniyi8@gmail.com>
2023-05-26 12:53:23 -0700

    header updated

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,28 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-/**
-* struct Division_t - structure for division
-*
-* @remainder: remainder after the div
-* @quotient:result after divic
-*/
-
-struct Division_t
-{
-	int remainder;
-	int quotient;
-};
-
-/**
- * Division - typedef for Division_t
- *
- */
-typedef struct Division_t Division;
-
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -30,8 +12,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-int _putchar(char c);
-
-
 
 #endif
