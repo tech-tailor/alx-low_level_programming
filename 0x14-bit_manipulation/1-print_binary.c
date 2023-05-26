fd0f5e36d8f188711ad4c467f1b97d6224082957
tech-tailor <ogundareakinniyi8@gmail.com>
2023-05-26 14:10:27 -0700

    updated function

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,54 +1,25 @@
 #include "main.h"
 
 /**
- *modulus_value- cal. quotient and remainder
- *@num: number argument
- *@divisor: divides the numer and it is 2
- *Return: quotient and reminder
+ * print_binary - prints the binary representation of a number
+ * @n: integer
+ * Return: void
  */
-
-Division modulus_value(int num, int divisor)
-{
-	Division result;
-
-	int count = 0;
-	int abs_value = 0;
-
-	abs_value = (num < 0) ? -num : num;
-	while (abs_value >= divisor)
-	{
-	abs_value -= divisor;
-	count++;
-	}
-	result.quotient = count;
-	result.remainder = (abs_value < 0) ? -abs_value : abs_value;
-	return (result);
-}
-/**
- *print_binary- print decimal to binary
- *@n: argument for the input num
- *Return: Success
- *
- */
-
 
 void print_binary(unsigned long int n)
 {
-	Division dec_bin;
+	int on, i;
+	unsigned long int x;
 
+	on = 0;
+	for (i = 63; i >= 0; i--)
+	{
+		x = (n >> i) & 1;
+		if (x == 1)
+			on = 1;
+		if (on == 1)
+			_putchar(((n >> i) & 1) + '0');
+	}
 	if (n == 0)
-	{
 		_putchar('0');
-		return;
-	}
-	else
-	{
-		while (n > 0)
-		{
-		dec_bin = modulus_value(n, 2);
-		_putchar('0' + dec_bin.remainder);
-		n = dec_bin.quotient;
-		}
-	}
 }
-
