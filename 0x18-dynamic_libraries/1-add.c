c25cfca05f4862c8d1ad4aabcb943d355577e423
Akin <ogundareakinniyi8@gmail.com>
2023-10-16 22:04:08 +0100

    dynamic lib

---
+++ 0x18-dynamic_libraries/1-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - sum two integers
+ * @a: a is an integer
+ * @b: b is an integer
+ *
+ * Return: integer
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
